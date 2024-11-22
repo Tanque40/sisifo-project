019d7e819149d9e9d4112a2b08d38666a76b725e
Bruno Vitte <bruno_vitte@hotmail.com>
2024-11-21 19:27:52 -0600

    firsts draws

---
+++ src/app/container-check/page.tsx
@@ -0,0 +1,5 @@
+export default function ContainerCheck() {
+  return (
+    <div>Container-check</div>
+  )
+}
---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,32 @@
+interface NavElementOptions {
+  name: string;
+  link: string;
+}
+
+export default function Navbar() {
+  const menuOptions: NavElementOptions[] = [
+    { name: 'Encender/Apagar equipos', link: '/' },
+    { name: 'Revisión de seguridad', link: '/security-check' },
+    { name: 'Revisión de contenedores', link: '/container-check' }
+  ]
+
+  return (
+    <nav className="border-2 border-r-black h-screen">
+      <div className="flex h-screen items-center justify-center">
+        <div className="h-1/4 w-3/4 flex flex-col justify-between ">
+          {menuOptions.map((option: NavElementOptions) => (
+            <a
+              href={option.link}
+              key={option.name}
+              className="h-auto border-2 border-gray-500 py-2 px-4 rounded-md text-center"
+            >
+              <div className="mt-1.5">
+                {option.name}
+              </div>
+            </a>
+          ))}
+        </div>
+      </div>
+    </nav>
+  )
+}
